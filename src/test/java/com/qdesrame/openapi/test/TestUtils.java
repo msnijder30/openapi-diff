0093e7ffb7620b381a3586bcd642be31df0ace89
Mark Snijder <snijderd@gmail.com>
2019-01-15 09:42:59 +0100

    Fix for #3, replaced maven-assembly-plugin with maven-shade-plugin. Fixed indentation for other files so it's uniform with the rest of the code.

--- src/test/java/com/qdesrame/openapi/test/TestUtils.java
+++ src/test/java/com/qdesrame/openapi/test/TestUtils.java
@@ -3,40 +3,39 @@ package com.qdesrame.openapi.test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.slf4j.LoggerFactory.getLogger;
 
-import org.slf4j.Logger;
-
 import com.qdesrame.openapi.diff.OpenApiCompare;
 import com.qdesrame.openapi.diff.model.ChangedOpenApi;
+import org.slf4j.Logger;
 
 public class TestUtils {
-    public static final Logger LOG = getLogger(TestUtils.class);
+  public static final Logger LOG = getLogger(TestUtils.class);
 
-    public static void assertOpenApiAreEquals(String oldSpec, String newSpec) {
-        ChangedOpenApi changedOpenApi = OpenApiCompare.fromLocations(oldSpec, newSpec);
-        LOG.debug("Result: {}", changedOpenApi.isChanged().getValue());
-        assertThat(changedOpenApi.getNewEndpoints()).isEmpty();
-        assertThat(changedOpenApi.getMissingEndpoints()).isEmpty();
-        assertThat(changedOpenApi.getChangedOperations()).isEmpty();
-    }
+  public static void assertOpenApiAreEquals(String oldSpec, String newSpec) {
+    ChangedOpenApi changedOpenApi = OpenApiCompare.fromLocations(oldSpec, newSpec);
+    LOG.debug("Result: {}", changedOpenApi.isChanged().getValue());
+    assertThat(changedOpenApi.getNewEndpoints()).isEmpty();
+    assertThat(changedOpenApi.getMissingEndpoints()).isEmpty();
+    assertThat(changedOpenApi.getChangedOperations()).isEmpty();
+  }
 
-    public static void assertOpenApiChangedEndpoints(String oldSpec, String newSpec) {
-        ChangedOpenApi changedOpenApi = OpenApiCompare.fromLocations(oldSpec, newSpec);
-        LOG.debug("Result: {}", changedOpenApi.isChanged().getValue());
-        assertThat(changedOpenApi.getNewEndpoints()).isEmpty();
-        assertThat(changedOpenApi.getMissingEndpoints()).isEmpty();
-        assertThat(changedOpenApi.getChangedOperations()).isNotEmpty();
-    }
+  public static void assertOpenApiChangedEndpoints(String oldSpec, String newSpec) {
+    ChangedOpenApi changedOpenApi = OpenApiCompare.fromLocations(oldSpec, newSpec);
+    LOG.debug("Result: {}", changedOpenApi.isChanged().getValue());
+    assertThat(changedOpenApi.getNewEndpoints()).isEmpty();
+    assertThat(changedOpenApi.getMissingEndpoints()).isEmpty();
+    assertThat(changedOpenApi.getChangedOperations()).isNotEmpty();
+  }
 
-    public static void assertOpenApiBackwardCompatible(
-            String oldSpec, String newSpec, boolean isDiff) {
-        ChangedOpenApi changedOpenApi = OpenApiCompare.fromLocations(oldSpec, newSpec);
-        LOG.debug("Result: {}", changedOpenApi.isChanged().getValue());
-        assertThat(changedOpenApi.isCompatible()).isTrue();
-    }
+  public static void assertOpenApiBackwardCompatible(
+      String oldSpec, String newSpec, boolean isDiff) {
+    ChangedOpenApi changedOpenApi = OpenApiCompare.fromLocations(oldSpec, newSpec);
+    LOG.debug("Result: {}", changedOpenApi.isChanged().getValue());
+    assertThat(changedOpenApi.isCompatible()).isTrue();
+  }
 
-    public static void assertOpenApiBackwardIncompatible(String oldSpec, String newSpec) {
-        ChangedOpenApi changedOpenApi = OpenApiCompare.fromLocations(oldSpec, newSpec);
-        LOG.debug("Result: {}", changedOpenApi.isChanged().getValue());
-        assertThat(changedOpenApi.isIncompatible()).isTrue();
-    }
+  public static void assertOpenApiBackwardIncompatible(String oldSpec, String newSpec) {
+    ChangedOpenApi changedOpenApi = OpenApiCompare.fromLocations(oldSpec, newSpec);
+    LOG.debug("Result: {}", changedOpenApi.isChanged().getValue());
+    assertThat(changedOpenApi.isIncompatible()).isTrue();
+  }
 }
